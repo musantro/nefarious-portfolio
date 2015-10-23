5234489bc9ebdc84e87ad14eb1244c4c97814ee7
Antonio Rodríguez <musantrobeats@gmail.com>
2015-10-23 20:38:18 +0200

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/musantro/pen/yYvKQm.
+
+ Build a CodePen.io app that successfully reverse-engineers this: http://codepen.io/ThiagoFerreir4/full/eNMxEp
---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+$("#target3").addClass("animated fadeOut");
+
+$("nav ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 500, 'easeOutCubic', function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
